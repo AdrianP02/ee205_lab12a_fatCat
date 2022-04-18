7bba8bfea212b6703268aa1b854bfa29ec6d3a76
Adrian <apeng2@hawaii.edu>
2022-04-17 21:26:58 -1000

    Added some basic tests.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
+#include "Weight.h"
+
 #include <iostream>
 
+using namespace std;
+
+Weight hi;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Weight testCat;
+    testCat.dump();     // Should print a default cat with no set values
+
+    cout << Weight::convertWeight(2.7, Weight::SLUG, Weight::KILO); // Should convert our 2.7 slugs to kilos
+
+    testCat = Weight(2.7, Weight::SLUG, 25);
+    testCat.dump();         // Should print a cat with the set values
+
+    Weight::UnitOfWeight unitOfWeight = Weight::SLUG;
+    cout << unitOfWeight;               // Should print the unit of weight. Slug = 2
+
+
+
+//    std::cout << Weight::convertWeight(3.0, Weight::KILO, Weight::POUND);
+//    hi = Weight(3.0, Weight::KILO, 300);
+//    Weight eknl = Weight(4.1, Weight::SLUG, 30);
+//    Weight::UnitOfWeight unitOfWeight = Weight::KILO;
+//    std::cout << unitOfWeight ;
+//    hi.dump();
+//    eknl.dump();
+//    Weight nan = Weight();
+//    nan.dump();
+
     return 0;
 }
